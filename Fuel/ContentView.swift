69e30d16604616639136af2e1778021f1124a174
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2022-01-11 23:04:07 -0600

    First commit

---
+++ Fuel/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Fuel
+//
+//  Created by Luis Mauricio Esparza Vazquez on 11/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ Fuel/FuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelApp.swift
+//  Fuel
+//
+//  Created by Luis Mauricio Esparza Vazquez on 11/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
