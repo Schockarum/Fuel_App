190d5224839883cdd53f6c24968fb44c54b4f68c
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2022-01-24 18:18:02 -0600

    Vista de usuario más unificada para mostrar toda la info en una pantalla

--- Fuel/Models/HealthData.swift
+++ Fuel/Models/HealthData.swift
@@ -11,6 +11,49 @@ struct HealthData {
     var fatPercentage: Float
     var hoursOfExcercise: Float
     var weight: Float
+    var dietObjective: DietObjective
+    
+    // MARK: - COMPUTED PROPERTIES
+    var proteinIntakeObjective: Float {
+        get {
+            var protein: Float
+            switch dietObjective {
+            case .cutting:
+                protein = weight * 2.6
+            case .mantaining, .bulking:
+                protein = weight * 2.2
+            }
+            return protein * 4
+        }
+    }
+    var fatIntakeObjective: Float {
+        get {
+            var fat: Float
+            switch dietObjective {
+            case .cutting:
+                fat = weight * 0.44
+            case .mantaining:
+                fat = weight * 0.77
+            case .bulking:
+                fat = weight * 0.88
+            }
+            return fat * 9
+        }
+    }
+    var carbIntakeObjective: Float {
+        get {
+            var carbs: Float
+            switch dietObjective {
+            case .cutting:
+                carbs = weight * 2.2
+            case .mantaining:
+                carbs = weight * 3.5
+            case .bulking:
+                carbs = weight * 4.4
+            }
+            return carbs * 4
+        }
+    }
     var leanBodyMass: Float {
         get {
             let lbm = ((1-fatPercentage/100)*weight)
@@ -25,14 +68,37 @@ struct HealthData {
     }
     var totalDailyEnergyExpenditure: Int {
         get {
-            let tdee = basalMetabolicRate * hoursOfExcercise
+            var k: Float
+            switch hoursOfExcercise {
+            case 0.0..<3.0:
+                k = 1.2
+            case 3.0..<6.0:
+                k = 1.35
+            default:
+                k = 1.5
+            }
+            let tdee = basalMetabolicRate * k
             return Int(round(tdee))
         }
     }
-
+    
+    
+    // MARK: - FUNCTIONS
     init(weight w: Float = 50.0, fatPercentage fp: Float = 15.0, hoursOfExcercisePerWeek hours: Float = 1.0){
         weight = w
         fatPercentage = fp
         hoursOfExcercise = hours
+        dietObjective = DietObjective.mantaining
+    }
+    
+    func getCalorieTotal() -> Float{
+        let calorieTotal = proteinIntakeObjective + carbIntakeObjective + fatIntakeObjective
+        return calorieTotal
+    }
+    
+    enum DietObjective{
+        case cutting
+        case mantaining
+        case bulking
     }
 }
--- Fuel/Views/UserProfileView.swift
+++ Fuel/Views/UserProfileView.swift
@@ -13,43 +13,123 @@ struct UserProfileView: View {
     let frameHeight: CGFloat = 50.0
     
     var body: some View {
-        VStack {
-            Image("Banner")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 425, height: 160)
-                .clipped()
-                .overlay {
-                    RoundedRectangle(cornerRadius: 5).stroke(.white, lineWidth: 5)
-                }
-                .ignoresSafeArea()
-                .offset(y: -50)
-                .padding(.bottom)
-            HStack {
-                Image("ProfilePic")
-                    .resizable(resizingMode: .stretch)
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle())
-                    .overlay{
-                        Circle().stroke(.white, lineWidth: 5)
+        ScrollView {
+            VStack {
+                // MARK: - Banner
+                Image("Banner")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 200)
+                    .clipped()
+                    .overlay {
+                        Rectangle().stroke(.white, lineWidth: 5)
                     }
-                    .padding(.leading)
-                VStack(alignment: .leading) {
-                    Text("Shortie ⚡️")
-                        .font(.largeTitle)
-                        .fontWeight(.medium)
-                        .minimumScaleFactor(0.5)
-                        .frame(height: 50)
-                        .padding([.top, .trailing])
-                    Text("Dripping with style")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
+                //                .ignoresSafeArea()
+                //                .offset(y: -51)
+                    .padding(.bottom)
+                // MARK: - Foto de Perfil y Nombre
+                HStack {
+                    Image("ProfilePic")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .overlay{
+                            Circle().stroke(.white, lineWidth: 5)
+                        }
+                        .padding(.leading)
+                    VStack(alignment: .leading) {
+                        Text("Shortie ⚡️")
+                            .font(.largeTitle)
+                            .fontWeight(.medium)
+                            .minimumScaleFactor(0.5)
+                            .frame(height: 30)
+                            .padding([.top, .trailing])
+                        Text("Dripping with style")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        //                        .offset(y: -20)
+                    }
+                    //                .offset(x: 10, y: 25)
+                    Spacer()
                 }
-                .offset(x: 10, y: 25)
+                //            .offset(y: -115)
+                Divider()
+                //                .offset(y: -100)
+                
+                // MARK: - Objetivos Calóricos
+                VStack {
+                    HStack {
+                        Text("Caloric Objectives 🍲")
+                            .font(.system(size: 25, weight: .semibold, design: .rounded))
+                            .fontWeight(.medium)
+                            .padding(.leading)
+                        Spacer()
+                    }
+                    //                .offset(y: -100)
+                    Text(String(format:"%.2f Cal",currentUser.healthData.getCalorieTotal()))
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                        .padding([.leading, .bottom, .trailing])
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Text("Carbs")
+                                .fontWeight(.medium)
+                            Text(String(format:"%.2f Cal",currentUser.healthData.carbIntakeObjective))
+                        }
+                        Spacer()
+                        VStack {
+                            Text("Protein")
+                                .fontWeight(.medium)
+                            Text(String(format:"%.2f Cal",currentUser.healthData.proteinIntakeObjective))
+                        }
+                        Spacer()
+                        VStack {
+                            Text("Fats")
+                                .fontWeight(.medium)
+                            Text(String(format:"%.2f Cal",currentUser.healthData.fatIntakeObjective))
+                        }
+                        Spacer()
+                    }
+                    Divider()
+                    // MARK: - Datos del Usuario
+                    HStack {
+                        Text("Current Data")
+                            .font(.system(size: 25, weight: .semibold, design: .rounded))
+                            .fontWeight(.medium)
+                            .padding([.leading, .bottom])
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Text("Weight")
+                                .fontWeight(.bold)
+                            TextField("Current weight", value: $currentUser.healthData.weight, format: .number)
+                                .frame(width: 50, alignment: .center)
+                                .textFieldStyle(.roundedBorder)
+                            Spacer()
+                            Text("Lean Body Mass")
+                                .fontWeight(.bold)
+                            Text(String(format:"%.2f",currentUser.healthData.leanBodyMass))
+                        }
+                        Spacer()
+                        VStack {
+                            Text("Fat %")
+                                .fontWeight(.bold)
+                            TextField("Fat Percentage", value: $currentUser.healthData.fatPercentage, format: .number)
+                                .frame(width: 50, alignment: .center)
+                                .textFieldStyle(.roundedBorder)
+                            Spacer()
+                            Text("Basal Metabolic Rate")
+                                .fontWeight(.bold)
+                            Text(String(format: "%.2f", currentUser.healthData.basalMetabolicRate))
+                        }
+                        Spacer()
+                    }
+                }
                 Spacer()
             }
-            .offset(y: -115)
-            Spacer()
         }
     }
 }
@@ -58,6 +138,7 @@ struct HealthData_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             UserProfileView()
+.previewInterfaceOrientation(.portrait)
         }
     }
 }
